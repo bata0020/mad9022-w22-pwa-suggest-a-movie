e91288a96dedeb73ae16e1457a6796e7a0dc873a
Leo Batan <leobatan@Leos-MacBook-Pro.local>
2022-02-24 08:56:52 -0500

    Add checkDB function. Remove createTx function

--- js/app.js
+++ js/app.js
@@ -37,13 +37,59 @@ const APP = {
             console.warn(err);
         });
     },
-    createTx: (storeName) => {
-        let tx = APP.DB.transaction(storeName, 'readwrite');
-        tx.addEventListener('error', (err) => {
-            console.warn(err);
-        })
-    },
+    // createTx: (storeName) => {
+    //     let tx = APP.DB.transaction(storeName, 'readwrite');
+    //     tx.addEventListener('error', (err) => {
+    //         console.warn(err);
+    //     });
+    //     tx.addEventListener('success', (ev) => {
+    //         console.log(ev.target.result);
+    //     });
+    //     tx.addEventListener('complete', (ev) => {
+    //         console.log(ev.target.result);
+    //     });
+    //     return tx;
+    // },
     addListeners: () => {
+        document.getElementById('searchForm').addEventListener('submit', APP.searchSubmitted);
+    },
+    searchSubmitted: (ev) => {
+        ev.preventDefault();
+        let input = document.getElementById('search').value.trim();
+        if (!input) return;
+        APP.checkDB(input);
+    },
+    checkDB: (keyword) => {
+        // checks indexeddb for match keyword
+        let tx = APP.DB.transaction('searchStore');
+        tx.addEventListener('complete', (ev) => {
+            console.log(ev.target.result);
+            console.log('Tx Complete. Do a fetch');
+            // what is next step?
+            APP.fetchMovieDB(keyword);
+        });
+        let store = tx.objectStore('searchStore');
+        let check = store.get(keyword);
+        check.addEventListener('success', (ev) => {
+            let checkResult = ev.target.result;
+            if (checkResult === undefined) {
+                console.log(`Check success. ${keyword} is not found in searchStore`);
+                // if does not exists do a fetch from api
+            } else {
+                console.log(`${keyword} exists in searchStore`);
+            }
+        },
+        { once: true }
+        );
+        check.addEventListener('error', (err) => {
+            console.warn(err);
+        });
+        check.addEventListener('complete', (ev) => {
+            console.log(`${keyword} exists in searchStore`);
+            console.log(ev.target.results);
+        });
+    },
+    fetchMovieDB: () => {
         
     },
 }
