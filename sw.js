d331ed5990d13362368770a1543a021c462290e5
Leo Batan <leobatan@Leos-MacBook-Pro.local>
2022-02-26 14:58:11 -0500

    Update fetch listener in SW

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const version = 6;
+const version = 7;
 const staticName = `PWA-Static-Movie-APP-${version}`;
 const dynamicName = `PWA-Dynamic-Movie-APP-${version}`;
 const cacheLimit = 40;
@@ -66,7 +66,6 @@ self.addEventListener('fetch', (ev) => {
                     })
                     .catch((err) => {
                         console.log('Fetch failed.', err);
-                        console.log(fetRes.status);
                         if (ev.request.mode == 'navigate') {
                             return caches.match('/404.html').then((page404Response) => {
                                 return page404Response;
