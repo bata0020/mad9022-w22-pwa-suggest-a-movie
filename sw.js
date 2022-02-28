1b2dfcf3f61d065ebebe386c0414a96c515b4dbf
Leo Batan <leobatan@Leos-MacBook-Pro.local>
2022-02-28 14:46:15 -0500

    Add send message function

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const version = 1;
+const version = 3;
 const staticName = `PWA-Static-Movie-APP-${version}`;
 const dynamicName = `PWA-Dynamic-Movie-APP-${version}`;
 const cacheLimit = 40;
@@ -67,7 +67,8 @@ self.addEventListener('fetch', (ev) => {
                     })
                     .catch((err) => {
                         console.log('Fetch failed.', err);
-                        if (err || ev.request.mode == 'navigate') {
+                        sendMessage(err);
+                        if (ev.request.mode == 'navigate') {
                             return caches.match('/404.html').then((page404Response) => {
                                 return page404Response;
                             })
